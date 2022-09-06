3f8bac89e75533d26a7a2969165b0b320dd4bc46
afauzann <afauzan857@gmail.com>
2022-09-07 06:03:40 +0700

    08_JS/Praktikum: revisi soal1.js

--- 08_Javascript_refreshment/Praktikum/soal1.js
+++ 08_Javascript_refreshment/Praktikum/soal1.js
@@ -1,8 +1,8 @@
-var a = "kampus merdeka";
+let a = "kampus merdeka";
 let b = "kampus merdeka";
 const nama = "Budi";
 let terdaftar = true;
-let lengkap_arr = [a, b, nama, terdaftar];
+let lengkapArr = [a, b, nama, terdaftar];
 
 function perkenalan() {
     let asal = "indonesia";
@@ -28,7 +28,7 @@ nama = b;
 */
 
 //console.log("asal diakses = " + asal);
-console.log("array = " + (lengkap_arr[2]));
+console.log("array = " + (lengkapArr[2]));
 console.log("a adalah = " + a);
 console.log("b adalah = " + b);
 perkenalan()
